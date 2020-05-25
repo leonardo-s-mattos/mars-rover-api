4a5fc5acc08ac29acd1d611a7d869dbb21548a98
Leonardo Mattos <leonardo.mattos@gmail.com>
2020-05-24 21:03:07 -0400

    Adding Eureka

--- src/main/java/com/mattos/marsrover/MarsRoverApiApplication.java
+++ src/main/java/com/mattos/marsrover/MarsRoverApiApplication.java
@@ -1,12 +1,13 @@
 package com.mattos.marsrover;
 
-import com.mattos.marsrover.application.service.RoverControlService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
+@EnableEurekaClient
 public class MarsRoverApiApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/mattos/marsrover/application/service/RoverControlServiceMust.java
+++ src/test/java/com/mattos/marsrover/application/service/RoverControlServiceMust.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
